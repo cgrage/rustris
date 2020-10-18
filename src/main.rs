f8dab87a7d693355889dbee03d406d00cf32c9ac
Christian Grage <christian.grage@gmx.de>
2020-10-18 16:55:22 +0200

    Playable

--- src/main.rs
+++ src/main.rs
@@ -50,8 +50,10 @@ enum CellVal {
   Free,
   Garbage,
   ActivePiece,
+  OutOfBoard,
 }
 
+#[derive(Copy, Clone)]
 enum BlockType {
   I,
   O,
@@ -60,6 +62,21 @@ enum BlockType {
   L,
   S,
   Z,
+}
+
+impl BlockType {
+  fn rand() -> BlockType {
+    let mut rnd = rand::thread_rng();
+    return match rnd.gen_range(0, 7) {
+      0 => BlockType::I,
+      1 => BlockType::O,
+      2 => BlockType::T,
+      3 => BlockType::J,
+      4 => BlockType::L,
+      5 => BlockType::S,
+      _ => BlockType::Z,
+    };
+  }
 }
 
 enum BlockRot {
@@ -75,6 +92,7 @@ struct Board {
   time: u32,
   time_to_step: u32,
   step_interval: u32,
+  next_block_type: BlockType,
 }
 
 impl Board {
@@ -85,6 +103,7 @@ impl Board {
       time: 0,
       time_to_step: 10,
       step_interval: 10,
+      next_block_type: BlockType::rand(),
     };
   }
 
@@ -96,27 +115,53 @@ impl Board {
     return BOARD_DIM_Y as i32;
   }
 
-  fn at(&self, x: i32, y: i32) -> &CellVal {
+  fn cell_value(&self, x: i32, y: i32) -> &CellVal {
     if self.block.probe_is_filled(x, y) {
       return &CellVal::ActivePiece;
+    }
+    return self.cell_value_raw(x, y);
+  }
+
+  fn cell_value_raw(&self, x: i32, y: i32) -> &CellVal {
+    if x < 0 || x >= BOARD_DIM_X as i32 || y < 0 || y >= BOARD_DIM_Y as i32 {
+      return &CellVal::OutOfBoard;
     }
     return &self.cells[y as usize][x as usize];
   }
 
-  fn move_block_vert(&mut self, amount : i32) {
-    self.block.move_block(amount, 0)
+  fn add_garbage(&mut self, x: i32, y: i32) {
+    self.cells[y as usize][x as usize] = CellVal::Garbage;
   }
 
-  fn lower_block(&mut self) {
-    self.block.move_block(0, 1)
+  fn collides(&self) -> bool {
+    // only check a 4x4 area at the blocks pos
+    for y in self.block.b_pos.1..self.block.b_pos.1 + 4 {
+      for x in self.block.b_pos.0..self.block.b_pos.0 + 4 {
+        if self.block.probe_is_filled(x, y) {
+          match self.cell_value_raw(x, y) {
+            CellVal::Free => (/* free */),
+            _ => return true,
+          }
+        }
+      }
+    }
+    return false;
   }
 
-  fn drop_block(&mut self) {
-    //
+  fn freeze_block(&mut self) {
+    // only check a 4x4 area at the blocks pos
+    for y in self.block.b_pos.1..self.block.b_pos.1 + 4 {
+      for x in self.block.b_pos.0..self.block.b_pos.0 + 4 {
+        if self.block.probe_is_filled(x, y) {
+          self.add_garbage(x, y);
+        }
+      }
+    }
   }
 
-  fn rotate_block(&mut self, amount : i32) {
-    self.block.rotate_block(amount);
+  fn next_block(&mut self) {
+    self.block.next(&self.next_block_type);
+    self.next_block_type = BlockType::rand();
   }
 }
 
@@ -203,7 +248,7 @@ impl UI {
     let b = UI::BOARD_BORDER;
     for y in 0..board.height() {
       for x in 0..board.width() {
-        let val = board.at(x, y);
+        let val = board.cell_value(x, y);
         self.board_win.mv(b + y, b + 2 * x);
         self.board_win.printw(self.cell_string(&val));
       }
@@ -215,8 +260,9 @@ impl UI {
   fn cell_string(&self, val: &CellVal) -> &str {
     return match val {
       CellVal::Free => "  ",
-      CellVal::Garbage => "XX",
+      CellVal::Garbage => "[]",
       CellVal::ActivePiece => "[]",
+      CellVal::OutOfBoard => "??",
     };
   }
 }
@@ -230,7 +276,7 @@ impl Game {
 
   fn step(&self, board: &mut Board) {
     if board.time_to_step == 0 {
-      board.block.move_block(0, 1);
+      self.lower_block(board);
       board.time_to_step = board.step_interval;
     } else {
       board.time_to_step = board.time_to_step - 1;
@@ -240,14 +286,52 @@ impl Game {
 
   fn handle_input(&self, input: &UserInput, board: &mut Board) {
     match input {
-      UserInput::MoveLeft => board.move_block_vert(-1),
-      UserInput::MoveRight => board.move_block_vert(1),
-      UserInput::MoveDown => board.lower_block(),
-      UserInput::DropDown => board.drop_block(),
-      UserInput::RotateLeft => board.rotate_block(-1),
-      UserInput::RotateRight => board.rotate_block(1),
+      UserInput::MoveLeft => self.move_block_vert(board, -1),
+      UserInput::MoveRight => self.move_block_vert(board, 1),
+      UserInput::MoveDown => self.lower_block(board),
+      UserInput::DropDown => self.drop_block(board),
+      UserInput::RotateLeft => self.rotate_block(board, -1),
+      UserInput::RotateRight => self.rotate_block(board, 1),
       _ => (),
     };
+  }
+
+  fn move_block_vert(&self, board: &mut Board, amount: i32) {
+    board.block.move_block(amount, 0);
+    if board.collides() {
+      // if we collide: undo action
+      board.block.move_block(-amount, 0);
+    }
+  }
+
+  fn rotate_block(&self, board: &mut Board, amount: i32) {
+    board.block.rotate_block(amount);
+    if board.collides() {
+      // if we collide: undo action
+      board.block.rotate_block(-amount);
+    }
+  }
+
+  fn lower_block(&self, board: &mut Board) {
+    board.block.move_block(0, 1);
+    if board.collides() {
+      // if we collide: undo action and FREEZE block
+      board.block.move_block(0, -1);
+      self.freeze_block_and_have_next(board);
+    }
+  }
+
+  fn drop_block(&self, board: &mut Board) {
+    while !board.collides() {
+      board.block.move_block(0, 1);
+    }
+    board.block.move_block(0, -1);
+    self.freeze_block_and_have_next(board);
+  }
+
+  fn freeze_block_and_have_next(&self, board: &mut Board) {
+    board.freeze_block();
+    board.next_block();
   }
 }
 
@@ -262,23 +346,16 @@ struct Block {
 impl Block {
   fn rand() -> Block {
     return Block {
-      b_type: Block::rand_block_type(),
+      b_type: BlockType::rand(),
       b_rot: BlockRot::Rot0,
       b_pos: (BOARD_DIM_X as i32 / 2, 0),
     };
   }
 
-  fn rand_block_type() -> BlockType {
-    let mut rnd = rand::thread_rng();
-    return match rnd.gen_range(0, 7) {
-      0 => BlockType::I,
-      1 => BlockType::O,
-      2 => BlockType::T,
-      3 => BlockType::J,
-      4 => BlockType::L,
-      5 => BlockType::S,
-      _ => BlockType::Z,
-    };
+  fn next(&mut self, block_type: &BlockType) {
+    self.b_type = *block_type;
+    self.b_rot = BlockRot::Rot0;
+    self.b_pos = (BOARD_DIM_X as i32 / 2, 0);
   }
 
   fn move_block(&mut self, x: i32, y: i32) {
