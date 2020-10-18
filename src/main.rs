06f24e86e0aad57594d1c42cc473b8206505302a
Christian Grage <christian.grage@gmx.de>
2020-10-18 18:30:11 +0200

    Refactoring

--- src/main.rs
+++ src/main.rs
@@ -6,15 +6,16 @@ use rand::Rng;
 fn main() {
   // println!("Starting Rustrix");
 
-  let mut board = Board::new(); // model
-  let ui = UI::new(); // view
-  let game = Game::new(); // controller
-
-  if !ui.has_enough_space() {
-    ui.destroy();
-    println!("UI init failed: {}", ui.space_error());
+  let ui_result = UI::new();
+  if ui_result.is_err() {
+    println!("UI init failed: {}", ui_result.unwrap_err());
     return;
   }
+
+  // checks done. let's go
+  let mut board = Board::new(); // model
+  let ui = ui_result.unwrap(); // view
+  let game = Game::new(); // controller
 
   loop {
     let user_input = ui.wait_for_user_input();
@@ -88,7 +89,9 @@ enum BlockRot {
 
 struct Board {
   cells: [[CellVal; BOARD_DIM_X]; BOARD_DIM_Y],
-  block: Block,
+  block_type: BlockType,
+  block_rot: BlockRot,
+  block_pos: (i32, i32),
   time: u32,
   time_to_step: u32,
   step_interval: u32,
@@ -99,7 +102,9 @@ impl Board {
   fn new() -> Board {
     return Board {
       cells: [[CellVal::Free; BOARD_DIM_X]; BOARD_DIM_Y],
-      block: Block::rand(),
+      block_type: BlockType::rand(),
+      block_rot: BlockRot::Rot0,
+      block_pos: (BOARD_DIM_X as i32 / 2, 0),
       time: 0,
       time_to_step: 10,
       step_interval: 10,
@@ -116,7 +121,7 @@ impl Board {
   }
 
   fn cell_value(&self, x: i32, y: i32) -> &CellVal {
-    if self.block.probe_is_filled(x, y) {
+    if self.probe_block_at(x, y) {
       return &CellVal::ActivePiece;
     }
     return self.cell_value_raw(x, y);
@@ -129,15 +134,59 @@ impl Board {
     return &self.cells[y as usize][x as usize];
   }
 
+  fn move_block(&mut self, x: i32, y: i32) {
+    self.block_pos = (self.block_pos.0 + x, self.block_pos.1 + y);
+  }
+
+  fn rotate_block(&mut self, r: i32) {
+    match r {
+      1 => {
+        self.block_rot = match self.block_rot {
+          BlockRot::Rot0 => BlockRot::Rot1,
+          BlockRot::Rot1 => BlockRot::Rot2,
+          BlockRot::Rot2 => BlockRot::Rot3,
+          BlockRot::Rot3 => BlockRot::Rot0,
+        }
+      }
+      -1 => {
+        self.block_rot = match self.block_rot {
+          BlockRot::Rot0 => BlockRot::Rot3,
+          BlockRot::Rot1 => BlockRot::Rot0,
+          BlockRot::Rot2 => BlockRot::Rot1,
+          BlockRot::Rot3 => BlockRot::Rot2,
+        }
+      }
+      _ => panic!("rotate_block must be called w/ 1 or -1"),
+    }
+  }
+
   fn add_garbage(&mut self, x: i32, y: i32) {
     self.cells[y as usize][x as usize] = CellVal::Garbage;
   }
 
+  fn probe_block_at(&self, board_x: i32, board_y: i32) -> bool {
+    if board_x - self.block_pos.0 < 0 || board_y - self.block_pos.1 < 0 {
+      return false;
+    }
+
+    let dx = (board_x - self.block_pos.0) as usize;
+    let dy = (board_y - self.block_pos.1) as usize;
+
+    return match self.block_type {
+      BlockType::I | BlockType::O => {
+        dx < 4 && dy < 4 && pattern_4x4(&self.block_type, &self.block_rot)[dy][dx] == 1
+      }
+      BlockType::T | BlockType::J | BlockType::L | BlockType::S | BlockType::Z => {
+        dx < 3 && dy < 3 && pattern_3x3(&self.block_type, &self.block_rot)[dy][dx] == 1
+      }
+    };
+  }
+
   fn collides(&self) -> bool {
     // only check a 4x4 area at the blocks pos
-    for y in self.block.b_pos.1..self.block.b_pos.1 + 4 {
-      for x in self.block.b_pos.0..self.block.b_pos.0 + 4 {
-        if self.block.probe_is_filled(x, y) {
+    for y in self.block_pos.1..self.block_pos.1 + 4 {
+      for x in self.block_pos.0..self.block_pos.0 + 4 {
+        if self.probe_block_at(x, y) {
           match self.cell_value_raw(x, y) {
             CellVal::Free => (/* free */),
             _ => return true,
@@ -150,9 +199,9 @@ impl Board {
 
   fn freeze_block(&mut self) {
     // only check a 4x4 area at the blocks pos
-    for y in self.block.b_pos.1..self.block.b_pos.1 + 4 {
-      for x in self.block.b_pos.0..self.block.b_pos.0 + 4 {
-        if self.block.probe_is_filled(x, y) {
+    for y in self.block_pos.1..self.block_pos.1 + 4 {
+      for x in self.block_pos.0..self.block_pos.0 + 4 {
+        if self.probe_block_at(x, y) {
           self.add_garbage(x, y);
         }
       }
@@ -160,13 +209,20 @@ impl Board {
   }
 
   fn next_block(&mut self) {
-    self.block.next(&self.next_block_type);
+    self.block_type = self.next_block_type;
+    self.block_rot = BlockRot::Rot0;
+    self.block_pos = (BOARD_DIM_X as i32 / 2, 0);
     self.next_block_type = BlockType::rand();
+  }
+
+  fn clear_full_rows(&self) -> i32 {
+    return 0;
   }
 }
 
+#[derive(Debug)]
 struct UI {
-  global_win: pancurses::Window,
+  screen: pancurses::Window,
   game_win: pancurses::Window,
   board_win: pancurses::Window,
 }
@@ -176,48 +232,57 @@ impl UI {
   const FULL_UI_WIDTH: i32 = 26;
   const FULL_UI_HEIGHT: i32 = 26;
 
-  fn new() -> UI {
-    let global_win = pancurses::initscr();
-    global_win.clear();
-    global_win.timeout(30);
-    global_win.keypad(true);
+  fn new() -> Result<UI, String> {
+    let screen = pancurses::initscr();
+
+    if screen.get_max_x() < UI::FULL_UI_WIDTH || screen.get_max_y() < UI::FULL_UI_HEIGHT {
+      pancurses::endwin();
+      return Err(format!(
+        "Not enough space in terminal; need {}x{}, have {}x{}",
+        UI::FULL_UI_WIDTH,
+        UI::FULL_UI_HEIGHT,
+        screen.get_max_x(),
+        screen.get_max_y()
+      ));
+    }
+
+    screen.clear();
+    screen.timeout(30);
+    screen.keypad(true);
     pancurses::noecho();
     pancurses::cbreak();
     pancurses::curs_set(0);
 
-    let game_win = match global_win.subwin(UI::FULL_UI_HEIGHT, UI::FULL_UI_WIDTH, 0, 0) {
-      Ok(win) => win,
-      Err(code) => panic!("pancurses subwin function failed w/ result code {}", code),
-    };
+    let game_win_result = screen.subwin(UI::FULL_UI_HEIGHT, UI::FULL_UI_WIDTH, 0, 0);
+    if game_win_result.is_err() {
+      pancurses::endwin();
+      return Err(format!(
+        "pancurses subwin function failed w/ result code {}",
+        game_win_result.unwrap_err()
+      ));
+    }
 
+    let game_win = game_win_result.unwrap();
     let board_with = (BOARD_DIM_X * 2) as i32 + 2 * UI::BOARD_BORDER;
     let board_height = BOARD_DIM_Y as i32 + 2 * UI::BOARD_BORDER;
-    let board_win = match game_win.subwin(board_height, board_with, 0, 0) {
-      Ok(win) => win,
-      Err(code) => panic!("pancurses subwin function failed w/ result code {}", code),
+
+    let board_win_result = game_win.subwin(board_height, board_with, 0, 0);
+    if board_win_result.is_err() {
+      pancurses::endwin();
+      return Err(format!(
+        "pancurses subwin function failed w/ result code {}",
+        board_win_result.unwrap_err()
+      ));
     };
+
+    let board_win = board_win_result.unwrap();
     board_win.border('|', '|', '-', '-', '+', '+', '+', '+');
 
-    return UI {
-      global_win: global_win,
+    return Ok(UI {
+      screen: screen,
       game_win: game_win,
       board_win: board_win,
-    };
-  }
-
-  fn has_enough_space(&self) -> bool {
-    return self.global_win.get_max_x() >= UI::FULL_UI_WIDTH
-      && self.global_win.get_max_y() >= UI::FULL_UI_HEIGHT;
-  }
-
-  fn space_error(&self) -> String {
-    return format!(
-      "Not enough space in terminal; need {}x{}, have {}x{}",
-      UI::FULL_UI_WIDTH,
-      UI::FULL_UI_HEIGHT,
-      self.global_win.get_max_x(),
-      self.global_win.get_max_y(),
-    );
+    });
   }
 
   fn destroy(&self) {
@@ -225,7 +290,7 @@ impl UI {
   }
 
   fn wait_for_user_input(&self) -> UserInput {
-    let ch = self.global_win.getch();
+    let ch = self.screen.getch();
     match ch {
       Some(pancurses::Input::Character('q')) => UserInput::UserWantsToQuit,
       Some(pancurses::Input::Unknown(27)) => UserInput::UserWantsToQuit, // 27 is ESC key
@@ -297,263 +362,199 @@ impl Game {
   }
 
   fn move_block_vert(&self, board: &mut Board, amount: i32) {
-    board.block.move_block(amount, 0);
+    board.move_block(amount, 0);
     if board.collides() {
       // if we collide: undo action
-      board.block.move_block(-amount, 0);
+      board.move_block(-amount, 0);
     }
   }
 
   fn rotate_block(&self, board: &mut Board, amount: i32) {
-    board.block.rotate_block(amount);
+    board.rotate_block(amount);
     if board.collides() {
       // if we collide: undo action
-      board.block.rotate_block(-amount);
+      board.rotate_block(-amount);
     }
   }
 
   fn lower_block(&self, board: &mut Board) {
-    board.block.move_block(0, 1);
+    board.move_block(0, 1);
     if board.collides() {
       // if we collide: undo action and FREEZE block
-      board.block.move_block(0, -1);
+      board.move_block(0, -1);
       self.freeze_block_and_have_next(board);
     }
   }
 
   fn drop_block(&self, board: &mut Board) {
     while !board.collides() {
-      board.block.move_block(0, 1);
+      board.move_block(0, 1);
     }
-    board.block.move_block(0, -1);
+    board.move_block(0, -1);
     self.freeze_block_and_have_next(board);
   }
 
   fn freeze_block_and_have_next(&self, board: &mut Board) {
     board.freeze_block();
+    /*let row_count =*/
+    board.clear_full_rows();
     board.next_block();
   }
 }
 
 // -----------------------------------------------------------------------------
 
-struct Block {
-  b_type: BlockType,
-  b_rot: BlockRot,
-  b_pos: (i32, i32),
-}
-
-impl Block {
-  fn rand() -> Block {
-    return Block {
-      b_type: BlockType::rand(),
-      b_rot: BlockRot::Rot0,
-      b_pos: (BOARD_DIM_X as i32 / 2, 0),
-    };
-  }
-
-  fn next(&mut self, block_type: &BlockType) {
-    self.b_type = *block_type;
-    self.b_rot = BlockRot::Rot0;
-    self.b_pos = (BOARD_DIM_X as i32 / 2, 0);
-  }
-
-  fn move_block(&mut self, x: i32, y: i32) {
-    self.b_pos = (self.b_pos.0 + x, self.b_pos.1 + y);
-  }
-
-  fn rotate_block(&mut self, r: i32) {
-    match r {
-      1 => {
-        self.b_rot = match self.b_rot {
-          BlockRot::Rot0 => BlockRot::Rot1,
-          BlockRot::Rot1 => BlockRot::Rot2,
-          BlockRot::Rot2 => BlockRot::Rot3,
-          BlockRot::Rot3 => BlockRot::Rot0,
-        }
-      }
-      -1 => {
-        self.b_rot = match self.b_rot {
-          BlockRot::Rot0 => BlockRot::Rot3,
-          BlockRot::Rot1 => BlockRot::Rot0,
-          BlockRot::Rot2 => BlockRot::Rot1,
-          BlockRot::Rot3 => BlockRot::Rot2,
-        }
-      }
-      _ => panic!("rotate_block must be called w/ 1 or -1"),
-    }
-  }
-
-  fn probe_is_filled(&self, board_x: i32, board_y: i32) -> bool {
-    if board_x - self.b_pos.0 < 0 || board_y - self.b_pos.1 < 0 {
-      return false;
-    }
-
-    let dx = (board_x - self.b_pos.0) as usize;
-    let dy = (board_y - self.b_pos.1) as usize;
-
-    return match self.b_type {
-      BlockType::I | BlockType::O => {
-        dx < 4 && dy < 4 && Block::pattern_4x4(&self.b_type, &self.b_rot)[dy][dx] == 1
-      }
-      BlockType::T | BlockType::J | BlockType::L | BlockType::S | BlockType::Z => {
-        dx < 3 && dy < 3 && Block::pattern_3x3(&self.b_type, &self.b_rot)[dy][dx] == 1
-      }
-    };
-  }
-
-  // from: https://strategywiki.org/wiki/Tetris/Rotation_systems
-  fn pattern_4x4(b_type: &BlockType, b_rot: &BlockRot) -> [[i32; 4]; 4] {
-    return match b_type {
-      BlockType::I => match b_rot {
-        BlockRot::Rot0 => [
-          [0, 0, 0, 0], //
-          [1, 1, 1, 1],
-          [0, 0, 0, 0],
-          [0, 0, 0, 0],
-        ],
-        BlockRot::Rot1 => [
-          [0, 0, 1, 0], //
-          [0, 0, 1, 0],
-          [0, 0, 1, 0],
-          [0, 0, 1, 0],
-        ],
-        BlockRot::Rot2 => [
-          [0, 0, 0, 0], //
-          [0, 0, 0, 0],
-          [1, 1, 1, 1],
-          [0, 0, 0, 0],
-        ],
-        BlockRot::Rot3 => [
-          [0, 1, 0, 0], //
-          [0, 1, 0, 0],
-          [0, 1, 0, 0],
-          [0, 1, 0, 0],
-        ],
-      },
-      BlockType::O => [
+// from: https://strategywiki.org/wiki/Tetris/Rotation_systems
+fn pattern_4x4(b_type: &BlockType, b_rot: &BlockRot) -> [[i32; 4]; 4] {
+  return match b_type {
+    BlockType::I => match b_rot {
+      BlockRot::Rot0 => [
         [0, 0, 0, 0], //
-        [0, 1, 1, 0],
-        [0, 1, 1, 0],
+        [1, 1, 1, 1],
+        [0, 0, 0, 0],
         [0, 0, 0, 0],
       ],
-      _ => panic!("unknown 4x4 type"),
-    };
-  }
+      BlockRot::Rot1 => [
+        [0, 0, 1, 0], //
+        [0, 0, 1, 0],
+        [0, 0, 1, 0],
+        [0, 0, 1, 0],
+      ],
+      BlockRot::Rot2 => [
+        [0, 0, 0, 0], //
+        [0, 0, 0, 0],
+        [1, 1, 1, 1],
+        [0, 0, 0, 0],
+      ],
+      BlockRot::Rot3 => [
+        [0, 1, 0, 0], //
+        [0, 1, 0, 0],
+        [0, 1, 0, 0],
+        [0, 1, 0, 0],
+      ],
+    },
+    BlockType::O => [
+      [0, 0, 0, 0], //
+      [0, 1, 1, 0],
+      [0, 1, 1, 0],
+      [0, 0, 0, 0],
+    ],
+    _ => panic!("unknown 4x4 type"),
+  };
+}
 
-  fn pattern_3x3(b_type: &BlockType, b_rot: &BlockRot) -> [[i32; 3]; 3] {
-    return match b_type {
-      BlockType::T => match b_rot {
-        BlockRot::Rot0 => [
-          [0, 1, 0], //
-          [1, 1, 1],
-          [0, 0, 0],
-        ],
-        BlockRot::Rot1 => [
-          [0, 1, 0], //
-          [0, 1, 1],
-          [0, 1, 0],
-        ],
-        BlockRot::Rot2 => [
-          [0, 0, 0], //
-          [1, 1, 1],
-          [0, 1, 0],
-        ],
-        BlockRot::Rot3 => [
-          [0, 1, 0], //
-          [1, 1, 0],
-          [0, 1, 0],
-        ],
-      },
-      BlockType::J => match b_rot {
-        BlockRot::Rot0 => [
-          [0, 0, 0], //
-          [1, 1, 1],
-          [0, 0, 1],
-        ],
-        BlockRot::Rot1 => [
-          [0, 1, 0], //
-          [0, 1, 0],
-          [1, 1, 0],
-        ],
-        BlockRot::Rot2 => [
-          [0, 0, 0], //
-          [1, 0, 0],
-          [1, 1, 1],
-        ],
-        BlockRot::Rot3 => [
-          [0, 1, 1], //
-          [0, 1, 0],
-          [0, 1, 0],
-        ],
-      },
-      BlockType::L => match b_rot {
-        BlockRot::Rot0 => [
-          [0, 0, 0], //
-          [1, 1, 1],
-          [1, 0, 0],
-        ],
-        BlockRot::Rot1 => [
-          [1, 1, 0], //
-          [0, 1, 0],
-          [0, 1, 0],
-        ],
-        BlockRot::Rot2 => [
-          [0, 0, 0], //
-          [0, 0, 1],
-          [1, 1, 1],
-        ],
-        BlockRot::Rot3 => [
-          [0, 1, 0], //
-          [0, 1, 0],
-          [0, 1, 1],
-        ],
-      },
-      BlockType::S => match b_rot {
-        BlockRot::Rot0 => [
-          [0, 0, 0], //
-          [0, 1, 1],
-          [1, 1, 0],
-        ],
-        BlockRot::Rot1 => [
-          [1, 0, 0], //
-          [1, 1, 0],
-          [0, 1, 0],
-        ],
-        BlockRot::Rot2 => [
-          [0, 0, 0], //
-          [0, 1, 1],
-          [1, 1, 0],
-        ],
-        BlockRot::Rot3 => [
-          [1, 0, 0], //
-          [1, 1, 0],
-          [0, 1, 0],
-        ],
-      },
-      BlockType::Z => match b_rot {
-        BlockRot::Rot0 => [
-          [0, 0, 0], //
-          [1, 1, 0],
-          [0, 1, 1],
-        ],
-        BlockRot::Rot1 => [
-          [0, 0, 1], //
-          [0, 1, 1],
-          [0, 1, 0],
-        ],
-        BlockRot::Rot2 => [
-          [0, 0, 0], //
-          [1, 1, 0],
-          [0, 1, 1],
-        ],
-        BlockRot::Rot3 => [
-          [0, 0, 1], //
-          [0, 1, 1],
-          [0, 1, 0],
-        ],
-      },
-      _ => panic!("unknown 3x3 type"),
-    };
-  }
+fn pattern_3x3(b_type: &BlockType, b_rot: &BlockRot) -> [[i32; 3]; 3] {
+  return match b_type {
+    BlockType::T => match b_rot {
+      BlockRot::Rot0 => [
+        [0, 1, 0], //
+        [1, 1, 1],
+        [0, 0, 0],
+      ],
+      BlockRot::Rot1 => [
+        [0, 1, 0], //
+        [0, 1, 1],
+        [0, 1, 0],
+      ],
+      BlockRot::Rot2 => [
+        [0, 0, 0], //
+        [1, 1, 1],
+        [0, 1, 0],
+      ],
+      BlockRot::Rot3 => [
+        [0, 1, 0], //
+        [1, 1, 0],
+        [0, 1, 0],
+      ],
+    },
+    BlockType::J => match b_rot {
+      BlockRot::Rot0 => [
+        [0, 0, 0], //
+        [1, 1, 1],
+        [0, 0, 1],
+      ],
+      BlockRot::Rot1 => [
+        [0, 1, 0], //
+        [0, 1, 0],
+        [1, 1, 0],
+      ],
+      BlockRot::Rot2 => [
+        [0, 0, 0], //
+        [1, 0, 0],
+        [1, 1, 1],
+      ],
+      BlockRot::Rot3 => [
+        [0, 1, 1], //
+        [0, 1, 0],
+        [0, 1, 0],
+      ],
+    },
+    BlockType::L => match b_rot {
+      BlockRot::Rot0 => [
+        [0, 0, 0], //
+        [1, 1, 1],
+        [1, 0, 0],
+      ],
+      BlockRot::Rot1 => [
+        [1, 1, 0], //
+        [0, 1, 0],
+        [0, 1, 0],
+      ],
+      BlockRot::Rot2 => [
+        [0, 0, 0], //
+        [0, 0, 1],
+        [1, 1, 1],
+      ],
+      BlockRot::Rot3 => [
+        [0, 1, 0], //
+        [0, 1, 0],
+        [0, 1, 1],
+      ],
+    },
+    BlockType::S => match b_rot {
+      BlockRot::Rot0 => [
+        [0, 0, 0], //
+        [0, 1, 1],
+        [1, 1, 0],
+      ],
+      BlockRot::Rot1 => [
+        [1, 0, 0], //
+        [1, 1, 0],
+        [0, 1, 0],
+      ],
+      BlockRot::Rot2 => [
+        [0, 0, 0], //
+        [0, 1, 1],
+        [1, 1, 0],
+      ],
+      BlockRot::Rot3 => [
+        [1, 0, 0], //
+        [1, 1, 0],
+        [0, 1, 0],
+      ],
+    },
+    BlockType::Z => match b_rot {
+      BlockRot::Rot0 => [
+        [0, 0, 0], //
+        [1, 1, 0],
+        [0, 1, 1],
+      ],
+      BlockRot::Rot1 => [
+        [0, 0, 1], //
+        [0, 1, 1],
+        [0, 1, 0],
+      ],
+      BlockRot::Rot2 => [
+        [0, 0, 0], //
+        [1, 1, 0],
+        [0, 1, 1],
+      ],
+      BlockRot::Rot3 => [
+        [0, 0, 1], //
+        [0, 1, 1],
+        [0, 1, 0],
+      ],
+    },
+    _ => panic!("unknown 3x3 type"),
+  };
 }
